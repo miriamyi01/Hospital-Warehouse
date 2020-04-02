e953a51fc8a165b3187032242fdcc2e41bffbacf
Miriam Reyes <54131192+miriamyi01@users.noreply.github.com>
2020-04-01 20:48:57 -0600

    Rename Suministro.c to SuministroMedico (Pre-4).c

--- SuministroMedico (Pre-4).c
+++ SuministroMedico (Pre-4).c
@@ -2,15 +2,15 @@
 #include<time.h>
 #include<stdlib.h>
 
-/*Menú de proveedor*/
+/*MenÃº de proveedor*/
 void proveedor();
 void prove_entrada(), prove_lista(), prove_actual();
 void buscar(), buscar_id(), buscar_nombre();
-/*Menú del cliente*/
+/*MenÃº del cliente*/
 void cliente();
 void clien_buscar(), buscar_cid(), buscar_cnombre();
 void clien_entrada(), clien_lista(), clien_actual();
-/*Menú del medicamento*/
+/*MenÃº del medicamento*/
 void medicina();
 void medi_venta(), balance(), update_balance();
 void medi_entrada(), medi_buscar(), remanente();
@@ -18,7 +18,7 @@ void medi_entrada(), medi_buscar(), remanente();
 void ticket();
 /*Sobre el programa*/
 void sobre();
-/*Menú de Reporte*/
+/*MenÃº de Reporte*/
 void reporte_menu();
 void reporte();
 void venta_rpt(), venta_rpt_diaria();
@@ -179,7 +179,7 @@ int getclien_id(){
     return info_c.clien_id+1;
 }
 
-/*                           Bienvenida y menú                            */
+/*                           Bienvenida y menÃº                            */
 /*========================== Pantalla de Inicio ==========================*/
 void bienvenida(){
     system("cls");
@@ -190,7 +190,7 @@ void bienvenida(){
 		printf("\n\tCargando......");
 }
 
-/*============================ Menú Principal ===========================*/
+/*============================ MenÃº Principal ===========================*/
 void main_menu(){
     int op;
     char ch, o=162;
@@ -249,7 +249,7 @@ int main(){
 }
 
 /*============================== PROVEEDOR ==============================*/
-/*========================== Menú de Proveedor ==========================*/
+/*========================== MenÃº de Proveedor ==========================*/
 void proveedor(){
     int opp, u=163;
     do{
@@ -284,7 +284,7 @@ void proveedor(){
     while(opp!=5);
 }
 
-/*========================= Añadir Proveedor ==========================*/
+/*========================= AÃ±adir Proveedor ==========================*/
 void prove_entrada(){
     int id, op;
     char ch;
@@ -321,14 +321,14 @@ void prove_entrada(){
 			fprintf(fp,"\n");
 			system("cls");
 			printf("Proveedor a%cadido con %cxito",164, 130);
-			printf("Más proveedores [S/N]");
+			printf("MÃ¡s proveedores [S/N]");
 			ch=getche();
 		}
     }
     fclose(fp);
 }
 
-/*================ Actualizar Información del Proveedor ===============*/
+/*================ Actualizar InformaciÃ³n del Proveedor ===============*/
 void prove_actual(){
     int i, op;
     char ch;
@@ -384,7 +384,7 @@ void prove_actual(){
 }
 
 /*============================== CLIENTE ==============================*/
-/*========================== Menú de Cliente ==========================*/
+/*========================== MenÃº de Cliente ==========================*/
 void cliente(){
     int opc, u=163;
     do{
@@ -420,7 +420,7 @@ void cliente(){
 }
 
 /*============================ MEDICAMENTO ============================*/
-/*======================== Menú de Medicamento ========================*/
+/*======================== MenÃº de Medicamento ========================*/
 void medicina(){
     int opm, u=163;
     do{
@@ -456,7 +456,7 @@ void medicina(){
 }
 
 /*============================== REPORTE ==============================*/
-/*========================== Menú de Reportes =========================*/
+/*========================== MenÃº de Reportes =========================*/
 void reporte_menu(){
     int opr;
 	do{
